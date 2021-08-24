1a537f98a3a4438176e52423a15abf575bf97a5c
Ivan Luzinov <kepz@mail.ru>
2021-08-25 00:38:32 +0300

    Comments was edited

--- ubuntu20_props.sh
+++ ubuntu20_props.sh
@@ -1,31 +1,36 @@
 #!/usr/bin/env bash
 #set -xe #For debugging purposes
 
-if [ "$EUID" -ne 0 ] #The $EUID environment variable holds the current users's UID. Root's UID is 0. Or use id -u.
+#The $EUID environment variable holds the current users's UID. Root's UID is 0. Or use id -u.
+if [ "$EUID" -ne 0 ] 
 	then echo "Notice: Please run this script as root"
 	exit
 fi
 
+#Set timezone
 TZ='Europe/Moscow'
 echo $TZ > /etc/timezone
 
 apt install -y tzdata
-rm -f /etc/localtime #Avoid Ubuntu's bug
+rm -f /etc/localtime
+
 #Reconfigure in noninteractive mode
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && dpkg-reconfigure -f noninteractive tzdata && apt clean
 
-if [ $? -eq 0 ]
-	then echo -e "Set Timezone=$TZ - \033[32mOK\033[0m"
-	else echo "Set Timezone=$TZ - ERROR"
+if [ $? -eq 0 ]; then
+	echo -e "Set Timezone=$TZ - \033[32mOK\033[0m"
+else
+	echo "Set Timezone=$TZ - ERROR"
 fi
 
-update-locale LANG=ru_RU.UTF-8 2>/dev/null  # Set LANG=RU in /etc/default/locale
+#Set LANG=RU in /etc/default/locale
+update-locale LANG=ru_RU.UTF-8 2>/dev/null
 #grep -q for quiet/silent mode (no output) with exit code 0 if no errors
 grep -q "LANG=ru_RU.UTF-8" /etc/default/locale  && echo -e "Set LANG=ru_RU.UTF-8 - \033[32mOK\033[0m" || echo "Set LANG=ru_RU.UTF-8 - ERROR"
 
-sed -i "s/^#*Port 22/Port 3322/" /etc/ssh/sshd_config #Change the default SSH port
-sed -i "s/^#*PermitRootLogin .*/PermitRootLogin No/g" /etc/ssh/sshd_config #Deactivate the SSH root login
-#grep -E 'Port 3322|PermitRootLogin No' /etc/ssh/sshd_config
+#SSH server configuration
+sed -i "s/^#*Port 22/Port 3322/" /etc/ssh/sshd_config
+sed -i "s/^#*PermitRootLogin .*/PermitRootLogin No/g" /etc/ssh/sshd_config
 systemctl restart sshd.service
 
 #Add new user and set him passwd
@@ -38,7 +43,6 @@ else
 	echo 'support:pass123' | chpasswd
 fi
 
-#Grant sudo rights via sudo Group
-#usermod -a -G sudo username
+#Allow to start/stop/restart services as sudo without password
 echo 'Cmnd_Alias USER_SERVICES = /bin/systemctl start *,/bin/systemctl stop *,/bin/systemctl restart *,/bin/systemctl status *' >> /etc/sudoers
 echo 'support ALL=(ALL) NOPASSWD:USER_SERVICES' >> /etc/sudoers
